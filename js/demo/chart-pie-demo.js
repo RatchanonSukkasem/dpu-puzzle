573df2d84ac1319caa4f2380540512a6a3e7e49e
ratchanon <ratchanon.suk@sbacnon.ac.th>
2024-05-17 13:38:55 +0700

    first commit

---
+++ js/demo/chart-pie-demo.js
@@ -0,0 +1,40 @@
+// Set new default font family and font color to mimic Bootstrap's default styling
+Chart.defaults.global.defaultFontFamily = 'Nunito', '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
+Chart.defaults.global.defaultFontColor = '#858796';
+
+// Pie Chart Example
+var ctx = document.getElementById("myPieChart");
+var count2 = localStorage.getItem('count2Value');
+var count1 = localStorage.getItem('count1Value');
+
+var myPieChart = new Chart(ctx, {
+  type: 'doughnut',
+  data: {
+    labels: [ "ผู้เล่นที่เล่นผ่าน", "ผู้เล่นที่ยังเล่นไม่ผ่าน"],
+    datasets: [{
+      
+      data: [count1, count2],
+      backgroundColor: ['#4e73df', '#1cc88a', '#36b9cc'],
+      hoverBackgroundColor: ['#2e59d9', '#17a673', '#2c9faf'],
+      hoverBorderColor: "rgba(234, 236, 244, 1)",
+    }],
+  },
+  options: {
+    maintainAspectRatio: false,
+    tooltips: {
+      backgroundColor: "rgb(255,255,255)",
+      bodyFontColor: "#858796",
+      borderColor: '#dddfeb',
+      borderWidth: 1,
+      xPadding: 15,
+      yPadding: 15,
+      displayColors: false,
+      caretPadding: 10,
+    },
+    legend: {
+      display: false
+    },
+    cutoutPercentage: 80,
+  },
+});
+
